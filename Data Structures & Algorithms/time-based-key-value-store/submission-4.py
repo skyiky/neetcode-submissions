074778028e2b352d6414d71adb3670be699bb1ff
David Zhang <davidzhang@microsoft.com>
2026-08-28 16:25:42 -0700

    Add: time-based-key-value-store - submission-4

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-4.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+class TimeMap:
+
+    def __init__(self):
+        self.hmap = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.hmap[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        arr = self.hmap[key]
+        l, r = 0, len(arr)-1
+        ans = ""
+        while l <= r: # a valid answer is not guaranteed
+            m = (r+l)//2
+            if arr[m][0] == timestamp: # early return
+                return arr[m][1]
+            if arr[m][0] > timestamp:
+                r = m - 1
+            else:
+                ans = arr[m][1]
+                l = m + 1
+        return ans
+
+
+        
