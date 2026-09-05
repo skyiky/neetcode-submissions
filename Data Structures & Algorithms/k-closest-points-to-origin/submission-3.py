99a6cfec13040e36e509d4193ba4ffc9a2f7432d
David Zhang <davidzhang@microsoft.com>
2026-09-05 13:31:29 -0700

    Add: k-closest-points-to-origin - submission-3

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-3.py
@@ -0,0 +1,28 @@
+import heapq
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        # O(NlogK)
+        # return heapq.nsmallest(k, points, lambda p: math.sqrt(p[0]**2 + p[1]**2))
+
+        # def eucl(p: list) -> float:
+        #   return math.sqrt(p[0]**2 + p[1]**2)
+
+        # nsmallest implementation:
+        h = [] # max heap constrained to size k
+        for p in points:
+            d = p[0] ** 2 + p[1] ** 2 # optimize away math.sqrt
+            if len(h) < k: # fill up the heap until k
+                heapq.heappush(h, (-d, p))
+            elif d < -h[0][0]:
+                heapq.heapreplace(h, (-d, p))
+        return [p for _, p in h]
+            
+        
+
+
+
+
+
+
+
+    
